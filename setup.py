bdce1d8cbb1ee630f1e20fc700fa1ce633b99f95
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-06-23 16:37:03 +0200

    pip installable

---
+++ setup.py
@@ -0,0 +1,40 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+exec(open('nanostat/version.py').read())
+
+setup(
+    name='NanoStat',
+    version=__version__,
+    description='Calculate statistics for Oxford Nanopore sequencing data and alignments',
+    long_description='Calculate statistics for Oxford Nanopore sequencing data and alignments',
+    url='https://github.com/wdecoster/nanostat',
+    author='Wouter De Coster',
+    author_email='decosterwouter@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='nanopore sequencing statistics',
+    packages=find_packages(),
+    install_requires=['nanoget>=0.10.1', 'nanomath>=0.11.0'],
+    package_data={'NanoStat': []},
+    package_dir={'nanostat': 'nanostat'},
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'NanoStat=nanostat.NanoStat:main',
+        ],
+    },
+)
