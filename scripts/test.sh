4111efa372dd0b89c14b2c8645ac42bcd8f7c1d5
wdecoster <decosterwouter@gmail.com>
2018-02-15 11:44:59 +0100

    woop changed nanofilt to nanostat

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@ set -ev
 
 git clone https://github.com/wdecoster/nanotest.git
 
-NanoFilt -h
+NanoStat -h
 
 NanoStat --fastq nanotest/reads.fastq.gz --prefix tester
 NanoStat --bam nanotest/alignment.bam -o outdir
